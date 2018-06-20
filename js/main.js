ca308fd906239fc13efca6a668394a5d621fac1d
ogbeh <sogbeh@gmail.com>
2018-06-20 15:41:24 +0100

    add the inline text

--- js/main.js
+++ js/main.js
@@ -10,4 +10,27 @@ function newHead() {
     form.appendChild(inp);
 }
 
+let max_fields      = 10;
+let wrapper         = $(".input_fields_wrap");
+let add_button      = $(".add_field_button");
+let remove_button   = $(".remove_field_button");
 
+$(add_button).click(function(e){
+    e.preventDefault();
+    let total_fields = wrapper[0].childNodes.length;
+    if(total_fields < max_fields){
+        $(wrapper).append('<input type="text" name="answer[]" class="field-long" />');
+    }
+});
+$(remove_button).click(function(e){
+    e.preventDefault();
+    let total_fields = wrapper[0].childNodes.length;
+    if(total_fields>1){
+        wrapper[0].childNodes[total_fields-1].remove();
+    }
+});
+
+let app = angular.module('tempApp', []);
+app.controller('myCtrl', function ($scope) {
+    $scope.back = "This"
+});
